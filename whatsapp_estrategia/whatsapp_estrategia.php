603d94bed53083e6d32837bc43354b6506aa2a45
Luis Borges <41124525+luis-borges@users.noreply.github.com>
2024-07-18 17:16:14 -0300

    Update whatsapp_estrategia.php

--- whatsapp_estrategia/whatsapp_estrategia.php
+++ whatsapp_estrategia/whatsapp_estrategia.php
@@ -1,16 +1,16 @@
 <?php
  /**
- * Plugin Name: Whatsapp Estrategia 
- * Plugin URI:http://www.estrategiaconcursos.com.br 
- * Description:Plugin criado para acrescentar um módulo float de botão do personalizado do Whatsapp ao Divi Builder
+ * Plugin Name:Whatsapp Plugin 
+ * Plugin URI:https://luisborges.dev/
+ * Description:Plugin criado para acrescentar um módulo float de botão WhatsApp personalizável em qualquer Divi Theme.
  * Version:1.0 
- * Author:Lus Borges 
- * Author URI:http://www.estrategiaconcursos.com.br 
+ * Author: Luis Borges 
+ * Author URI:https://luisborges.dev/
  * License: GPL2
  * License URI: https://www.gnu.org/licenses/gpl-2.0.html    
-    Whatsapp Estrategia is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 2 of the License, or any later version. 
-    Whatsapp Estrategia is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details. 
-    You should have received a copy of the GNU General Public License along with Whatsapp Estrategia. If not, see https://www.gnu.org/licenses/gpl-2.0.html. 
+    Whatsapp Plugin is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 2 of the License, or any later version. 
+    Whatsapp Plugin is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details. 
+    You should have received a copy of the GNU General Public License along with Whatsapp Plugin. If not, see https://www.gnu.org/licenses/gpl-2.0.html. 
  * */
     // Exit if accessed directly
     if (!defined("ABSPATH")) {
